93f10828c7427a9b18423aee951ece17b840adf6
Алексей Московцев <alexmosgalaxy@gmail.com>
2024-10-24 20:07:54 +0300

    debug

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = os.getenv(
     'SECRET_KEY', 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
 )
 
-DEBUG = eval(os.getenv('DEBUG', 'True'))
+DEBUG = 'True' == os.getenv('DEBUG', 'True')
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '89.169.161.24,taskialexmos.zapto.org').split(',')
 
